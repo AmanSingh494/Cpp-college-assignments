420a7f599d5a38aa4e2c82d3a5e78322c5589642
AmanSingh494 <130276810+AmanSingh494@users.noreply.github.com>
2024-02-08 06:26:51 +0000

    Added wap3.cpp

---
+++ .ccls-cache/@home@runner@C-college-assignments/Assignment due 15feb@wap3.cpp
@@ -0,0 +1,11 @@
+/*3. WAP to compute the sum of the first n terms of the following series
+
+S = 1 + 1/2! + 1/3! + 1/4! +……*/
+#include <iostream>
+using namespace std;
+float sum(int n){
+  float total ;
+  for(int i = 1;i<=n;i++){
+    
+  }
+}
