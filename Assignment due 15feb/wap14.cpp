a59ada7c8de9f7c5af89188dc80865360ab858a7
AmanSingh494 <amanforwork1@gmail.com>
2024-02-20 10:44:19 +0530

    updated wap14

--- Assignment due 15feb/wap14.cpp
+++ Assignment due 15feb/wap14.cpp
@@ -1,82 +1,109 @@
-/*WAP to perform following actions on an array entered by the user: 
-Print the even-valued elements 
-Print the odd-valued elements 
-Calculate and print the sum and average of the elements of array 
-Print the maximum and minimum element of array 
-Remove the duplicates from the array 
+/*WAP to perform following actions on an array entered by the user:
+Print the even-valued elements
+Print the odd-valued elements
+Calculate and print the sum and average of the elements of array
+Print the maximum and minimum element of array
+Remove the duplicates from the array
 Print the array in reverse order*/
 #include <iostream>
 #include <algorithm>
 #include <set>
 using namespace std;
-void printEven(int arr[],int len){
-  cout << "The even valued-elements are"<< endl;
-  for(int j = 0;j<len;j++){
-    if(arr[j] %2==0){
+void printEven(int arr[], int len)
+{
+  cout << "The even valued-elements are ";
+  for (int j = 0; j < len; j++)
+  {
+    if (arr[j] % 2 == 0)
+    {
       cout << arr[j] << ' ';
     }
-    
   }
+  cout << endl;
 }
-void printOdd(int arr[],int len){
-  cout << "The odd valued-elements are"<< endl;
-  for(int j = 0;j<len;j++){
-    if(arr[j] %2!=0){
+void printOdd(int arr[], int len)
+{
+  cout << "The odd valued-elements are ";
+  for (int j = 0; j < len; j++)
+  {
+    if (arr[j] % 2 != 0)
+    {
       cout << arr[j] << ' ';
     }
-    
   }
+  cout << endl;
 }
-void printAvg(int arr[],int len){
-  
+void printAvg(int arr[], int len)
+{
+
   int sum = 0;
-  for(int j = 0;j<len;j++){
-    sum+=arr[j];
+  for (int j = 0; j < len; j++)
+  {
+    sum += arr[j];
   }
-  float avg = sum/len;
-  cout <<"The average and sum are "<< avg<< ' '<<sum << endl;
+  float avg = sum / len;
+  cout << "The average and sum are " << avg << ' ' << sum << endl;
 }
-void printMaxMin(int arr[],int len){
-  sort(arr,arr+len);
-  cout << "The max and min element are"<< arr[len-1]<<' '<<arr[0]<<endl; 
+void printMaxMin(int arr[], int len)
+{
+  sort(arr, arr + len);
+  cout << "The max and min element are " << arr[len - 1] << ' ' << arr[0] << endl;
 }
-void removeDupl(int arr[],int len){
-  set<int> noDupl ;
-  for(int i=0;i<len;i++){
+void removeDupl(int arr[], int len)
+{
+  set<int> noDupl;
+  for (int i = 0; i < len; i++)
+  {
     noDupl.insert(arr[i]);
   }
   int arrSize = noDupl.size();
   int newArr[arrSize];
-  for(int i=0;i<arrSize;i++){
-    auto it= noDupl.begin();
-    it += i;
-    arr[i]= *it;
+  int i = 0;
+  for (auto it = noDupl.begin(); it != noDupl.end(); it++)
+  {
+    arr[i] = *it;
+    i++;
   }
-  cout << "The array eith no duplicates is" << endl;
-  for(int i = 0;i<arrSize;i++){
+  cout << "The array with no duplicates is" << endl;
+  for (int i = 0; i < arrSize; i++)
+  {
     cout << newArr[i] << ' ';
   }
 }
-void fillArr(int arr[] , int size){
-  for(int i = 0;i<size; i++){
+
+void printReverse(int arr[], int len)
+{
+  cout << "The reversed array is ";
+  for (int i = len - 1; i >= 0; i--)
+  {
+    cout << arr[i] << ' ';
+  } 
+}
+
+void fillArr(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
     cin >> arr[i];
   }
 }
+
 int main()
 {
   int len1;
-  
-  cout << "Enter the number of elements in array 1"<< endl;
+
+  cout << "Enter the number of elements in array 1" << endl;
   cin >> len1;
-  
+
   int arr1[len1];
-  
-  
-  cout << "Enter the  elements in array 1"<< endl;
-  fillArr(arr1,len1);
-  
-  for(int i = 0;i<len1;i++){
-    cout << arr1[i];
-  }
-    return 0;
+
+  cout << "Enter the  elements in array 1" << endl;
+  fillArr(arr1, len1);
+  printEven(arr1, len1);
+  printOdd(arr1, len1);
+  printAvg(arr1, len1);
+  printMaxMin(arr1, len1);
+  removeDupl(arr1, len1);
+  printReverse(arr1, len1);
+  return 0;
 }
--- Assignment due 15feb/wap18.cpp
+++ Assignment due 15feb/wap18.cpp
@@ -1,19 +1,25 @@
-//Given two ordered arrays of integers, write a program to merge the two arrays to get an ordered array. 
+// Given two ordered arrays of integers, write a program to merge the two arrays to get an ordered array.
 #include <iostream>
 #include <algorithm>
 using namespace std;
-void mergeArr(int arr1[],int arr2[],int arr3[],int len1,int len2){
-  for(int i=0;i<len1+len2;i++){
-    if(i<len1){
+void mergeArr(int arr1[], int arr2[], int arr3[], int len1, int len2)
+{
+  for (int i = 0; i < len1 + len2; i++)
+  {
+    if (i < len1)
+    {
       arr3[i] = arr1[i];
     }
-    else{
-      arr3[i] = arr2[i-len1];
+    else
+    {
+      arr3[i] = arr2[i - len1];
     }
   }
 }
-void fillArr(int arr[] , int size){
-  for(int i = 0;i<size; i++){
+void fillArr(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
     cin >> arr[i];
   }
 }
@@ -21,22 +27,23 @@ int main()
 {
   int len1;
   int len2;
-  cout << "Enter the number of elements in array 1"<< endl;
+  cout << "Enter the number of elements in array 1" << endl;
   cin >> len1;
-  cout << "Enter the number of elements in array 2"<< endl;
+  cout << "Enter the number of elements in array 2" << endl;
   cin >> len2;
   int arr1[len1];
   int arr2[len2];
-  int mergedArr[len1+len2];
-  cout << "Enter the  elements in array 1"<< endl;
-  fillArr(arr1,len1);
-  cout << "Enter the  elements in array 2"<< endl;
-  fillArr(arr2,len2);
-  mergeArr(arr1,arr2,mergedArr,len1,len2);
+  int mergedArr[len1 + len2];
+  cout << "Enter the  elements in array 1" << endl;
+  fillArr(arr1, len1);
+  cout << "Enter the  elements in array 2" << endl;
+  fillArr(arr2, len2);
+  mergeArr(arr1, arr2, mergedArr, len1, len2);
   int n = sizeof(mergedArr) / sizeof(mergedArr[0]);
-  sort(mergedArr,mergedArr +n);
-  for(int i = 0;i<len1+len2;i++){
-    cout << mergedArr[i];
+  sort(mergedArr, mergedArr + n);
+  for (int i = 0; i < len1 + len2; i++)
+  {
+    cout << mergedArr[i] << ' ';
   }
-    return 0;
+  return 0;
 }
