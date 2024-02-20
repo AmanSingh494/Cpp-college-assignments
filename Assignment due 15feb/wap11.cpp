95fa0a615b04845c648d6711e4cc029def7d969a
AmanSingh494 <130276810+AmanSingh494@users.noreply.github.com>
2024-02-20 05:13:22 +0000

    Added wap 11

---
+++ Assignment due 15feb/wap11.cpp
@@ -0,0 +1,14 @@
+//Print a given number in words
+#include <iostream>
+using namespace std;
+void printInWords(int n){
+  int num[] = {' ',}
+ 
+}
+int main(){
+  int num ;
+  cout << "Enter the number"<< endl;
+  cin >> num;
+  printInWords(num);
+  return 0;
+}
